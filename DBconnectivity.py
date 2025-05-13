842860dbf773bc277a9e07c6b94853c63b76bf70
Shoaib Hamza <shoaibhamza14082000@gmail.com>
2025-05-13 10:09:15 +0500

    12233

--- DBconnectivity.py
+++ DBconnectivity.py
@@ -1,8 +1,9 @@
 import mysql.connector
+
 mydb = mysql.connector.connect
-{
-    host = "db4free.net",
+(
+    host="db4free.net",
     user = "stdmgm",
     password="*$=VJ7\"xVw8b^}n",
     database="studentmanage"
-}
+)
